aa098083c00d2a405eba4d6b249b81704c16eedf
Galezy25 <galezy25@gmail.com>
2022-03-25 03:14:58 -0600

    style: Font awesome cart and badge

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,6 @@
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { PropsWithChildren, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { CartState } from '../reducers/cart.reducer';
 import { RootState } from '../reducers/root.reducer';
@@ -11,12 +13,20 @@ function Layout({ children }: LayoutProps) {
   const [sidebarShow, setSidebarShow] = useState(false);
   const cart = useSelector<RootState, CartState>((root) => root.cart);
 
+  let totalAmount = useMemo(() => {
+    let result = 0;
+    cart.forEach((item) => {
+      result += item.amount;
+    });
+    return result;
+  }, [cart]);
+
   useEffect(() => {
     setSidebarShow(!!cart.length);
   }, [cart]);
   return (
     <div className="flex flex-col h-screen w-full overflow-hidden">
-      <nav className="w-full h-12 flex flex-row relative content-theme border-b border-theme">
+      <nav className="w-full h-14 flex flex-row relative content-theme border-b border-theme">
         <div className="ml-auto my-auto">
           <button
             className={`btn-theme ${
@@ -24,7 +34,12 @@ function Layout({ children }: LayoutProps) {
             }`}
             onClick={() => setSidebarShow((prev) => !prev)}
           >
-            Carrito
+            <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+            {!!totalAmount && (
+              <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+                {totalAmount}
+              </span>
+            )}
           </button>
         </div>
       </nav>
