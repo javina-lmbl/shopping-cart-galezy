6fc35364eabff4a6b656418817d23f1a47f4edda
Galezy25 <galezy25@gmail.com>
2022-03-24 22:14:54 -0600

    style: tailwind initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
+  theme: {
+    colors,
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
